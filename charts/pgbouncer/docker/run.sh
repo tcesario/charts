cb924f84078924bfa96e720b28048ef95a73f436
tcesario <62117677+tcesario@users.noreply.github.com>
2021-01-15 09:42:46 -0300

    Update run.sh

--- charts/pgbouncer/docker/run.sh
+++ charts/pgbouncer/docker/run.sh
@@ -5,10 +5,6 @@ cat <<EOF > /pgbouncer/users.txt
 "$PG_USER" "md5$password_checksummed"
 EOF
 
-cat << EOF > /pgbouncer/pgbouncer.config
-[databases]
-*=host=$PG_ADDR port=$PG_PORT user=$PG_USER password=$PG_PASS
-
 [pgbouncer]
 $(for i in $(find /pgbouncer-config/*);do echo "$(basename $i)=$(cat $i)";done)
 EOF
